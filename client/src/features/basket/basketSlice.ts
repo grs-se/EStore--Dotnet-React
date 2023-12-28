f0f7d1b36d83a887cfeb6994934e8d7f1719dc78
grs-se <george_ricesmith@hotmail.com>
2023-12-28 22:36:00 +0000

    Refactor app to use redux store 2

--- client/src/features/basket/basketSlice.ts
+++ client/src/features/basket/basketSlice.ts
@@ -1,13 +1,34 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import agent from "../../app/api/agent";
 import { Basket } from "../../app/models/basket";
 
 interface BasketState {
   basket: Basket | null;
+  status: string;
 }
 
 const initialState: BasketState = {
   basket: null,
+  status: "idle",
 };
+
+// createAsyncThunk = wrapper around a function which returns another function
+// they create actions on our behalf which we can then use to do something in our store
+export const addBasketItemAsync = createAsyncThunk<
+  Basket,
+  { productId: number; quantity?: number }
+>(
+  // type/nameofmethod
+  "basket/addBasketItemAsync",
+  // payload creator
+  async ({ productId, quantity = 1 }) => {
+    try {
+      return await agent.Basket.addItem(productId, quantity);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
 
 export const basketSlice = createSlice({
   name: "basket",
@@ -26,6 +47,21 @@ export const basketSlice = createSlice({
       if (state.basket?.items[itemIndex].quantity === 0)
         state.basket.items.splice(itemIndex, 1);
     },
+  },
+  // redux-toolkit removes a lot of boilerplate but still some boilerplate left
+  // complexity is abstracted away from us: magic
+  extraReducers: (builder) => {
+    builder.addCase(addBasketItemAsync.pending, (state, action) => {
+      //   console.log(action);
+      state.status = "pendingAddItem" + action.meta.arg.productId;
+    });
+    builder.addCase(addBasketItemAsync.fulfilled, (state, action) => {
+      state.basket = action.payload;
+      state.status = "idle";
+    });
+    builder.addCase(addBasketItemAsync.rejected, (state) => {
+      state.status = "idle";
+    });
   },
 });
 
