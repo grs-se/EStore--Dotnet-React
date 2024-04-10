8be85e8122adb0183dadce85a0ed08bb60637160
grs-se <george_ricesmith@hotmail.com>
2024-04-10 21:55:06 +0100

    Get the filter list from API and store in Redux state

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,24 +1,34 @@
-import { useEffect } from "react";
-import LoadingComponent from "../../app/layout/LoadingComponent";
-import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
-import ProductList from "./ProductList";
-import { fetchProductsAsync, productSelectors } from "./catalogSlice";
+import { useEffect } from 'react';
+import LoadingComponent from '../../app/layout/LoadingComponent';
+import { useAppDispatch, useAppSelector } from '../../app/store/configureStore';
+import ProductList from './ProductList';
+import {
+	fetchFilters,
+	fetchProductsAsync,
+	productSelectors,
+} from './catalogSlice';
 
 export default function Catalog() {
-  const products = useAppSelector(productSelectors.selectAll);
-  const { productsLoaded, status } = useAppSelector((state) => state.catalog);
-  const dispatch = useAppDispatch();
+	const products = useAppSelector(productSelectors.selectAll);
+	const { productsLoaded, status, filtersLoaded } = useAppSelector(
+		(state) => state.catalog
+	);
+	const dispatch = useAppDispatch();
 
-  useEffect(() => {
-    if (!productsLoaded) dispatch(fetchProductsAsync());
-  }, [dispatch, productsLoaded]);
+	useEffect(() => {
+		if (!productsLoaded) dispatch(fetchProductsAsync());
+	}, [productsLoaded, dispatch]);
 
-  if (status.includes("pending"))
-    return <LoadingComponent message="Loading products..." />;
+	useEffect(() => {
+		if (!filtersLoaded) dispatch(fetchFilters());
+	}, [dispatch, filtersLoaded]);
 
-  return (
-    <>
-      <ProductList products={products} />
-    </>
-  );
+	if (status.includes('pending'))
+		return <LoadingComponent message="Loading products..." />;
+
+	return (
+		<>
+			<ProductList products={products} />
+		</>
+	);
 }
