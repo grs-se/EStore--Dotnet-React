d1eea7429752674090cfe9bda8b442662a7413c1
grs-se <george_ricesmith@hotmail.com>
2024-04-14 19:37:11 +0100

    Create login form

--- client/src/features/account/Login.tsx
+++ client/src/features/account/Login.tsx
@@ -1,3 +1,95 @@
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { Paper } from '@mui/material';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+
 export default function Login() {
-	return <h1>Login</h1>;
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		const data = new FormData(event.currentTarget);
+		console.log({
+			email: data.get('email'),
+			password: data.get('password'),
+		});
+	};
+
+	return (
+		<Container
+			component={Paper}
+			maxWidth="sm"
+			sx={{
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center',
+				p: 4,
+			}}
+		>
+			<Box
+				sx={{
+					marginTop: 8,
+					display: 'flex',
+					flexDirection: 'column',
+					alignItems: 'center',
+				}}
+			>
+				<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+					<LockOutlinedIcon />
+				</Avatar>
+				<Typography
+					component="h1"
+					variant="h5"
+				>
+					Sign in
+				</Typography>
+				<Box
+					component="form"
+					onSubmit={handleSubmit}
+					noValidate
+					sx={{ mt: 1 }}
+				>
+					<TextField
+						margin="normal"
+						required
+						fullWidth
+						id="email"
+						label="Email Address"
+						name="email"
+						autoComplete="email"
+						autoFocus
+					/>
+					<TextField
+						margin="normal"
+						required
+						fullWidth
+						name="password"
+						label="Password"
+						type="password"
+						id="password"
+						autoComplete="current-password"
+					/>
+					<Button
+						type="submit"
+						fullWidth
+						variant="contained"
+						sx={{ mt: 3, mb: 2 }}
+					>
+						Sign In
+					</Button>
+					<Grid container>
+						<Grid item>
+							<Link to="/register">{"Don't have an account? Sign Up"}</Link>
+						</Grid>
+					</Grid>
+				</Box>
+			</Box>
+		</Container>
+	);
 }
+
