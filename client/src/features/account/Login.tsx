0ebaa380bff7cdd4e36aa728b59d07c901d9b1a6
grs-se <george_ricesmith@hotmail.com>
2024-04-14 22:23:58 +0100

    Use controlled component

--- client/src/features/account/Login.tsx
+++ client/src/features/account/Login.tsx
@@ -7,18 +7,22 @@ import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
-import * as React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import agent from '../../app/api/agent';
 
 export default function Login() {
-	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+	const [values, setValues] = useState({ username: '', password: '' });
+
+	const handleSubmit = (event: any) => {
 		event.preventDefault();
-		const data = new FormData(event.currentTarget);
-		console.log({
-			email: data.get('email'),
-			password: data.get('password'),
-		});
+		agent.Account.login(values);
 	};
+
+	function handleInputChange(event: any) {
+		const { name, value } = event.target;
+		setValues({ ...values, [name]: value });
+	}
 
 	return (
 		<Container
@@ -56,23 +60,21 @@ export default function Login() {
 				>
 					<TextField
 						margin="normal"
-						required
 						fullWidth
-						id="email"
-						label="Email Address"
-						name="email"
-						autoComplete="email"
+						label="Username"
+						name="username"
 						autoFocus
+						onChange={handleInputChange}
+						value={values.username}
 					/>
 					<TextField
 						margin="normal"
-						required
 						fullWidth
 						name="password"
 						label="Password"
 						type="password"
-						id="password"
-						autoComplete="current-password"
+						onChange={handleInputChange}
+						value={values.password}
 					/>
 					<Button
 						type="submit"
